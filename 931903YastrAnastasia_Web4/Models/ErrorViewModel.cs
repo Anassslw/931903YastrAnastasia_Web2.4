a9799c6f7fb076d0dfe0dbf024d7f317e8d82b28
Anassslw <91026718+Anassslw@users.noreply.github.com>
2022-05-08 20:37:19 +0700

    2.4

---
+++ 931903YastrAnastasia_Web4/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _931903YastrAnastasia_Web4.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
